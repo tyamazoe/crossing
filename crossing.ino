fea291339a9ee3ab6a0bd32f016d6b0254e85ea2
tyamazoe <tomohisa.yamazoe@gmail.com>
2018-09-01 13:31:21 +0900

    [update] Disabled mp3 and LED blinker.
    For debugging.

--- crossing.ino
+++ crossing.ino
@@ -2,18 +2,20 @@
 // Crossing
 // 2018-08-21 rev 0.1 initial
 // 2018-08-30 rev 0.2 mp3 working
+// 2018-09-01 rev 0.3 LED blinker
 //
 #include <M5Stack.h>
-#include <WiFi.h>
+//#include <WiFi.h>
 #include <Ultrasonic.h>
+/*
 #include "AudioFileSourceSD.h"
 #include "AudioFileSourceID3.h"
 #include "AudioGeneratorMP3.h"
 #include "AudioOutputI2S.h"
-
+*/
 
 #define ULTRASONIC_PIN  (22)
-#define INTERVAL        (200)
+#define INTERVAL        (300)
 #define SERVO_PIN       (5)
 #define LED1_PIN        (16)
 #define LED2_PIN        (17)
@@ -29,11 +31,13 @@ bool blinker = false;
 #define NOTE_D3 350
 */
 // MP3
+/*
 AudioGeneratorMP3 *mp3;
 AudioFileSourceSD *file;
 AudioOutputI2S *out;
 AudioFileSourceID3 *id3;
-
+*/
+/*
 void setupMP3() 
 {
   //mp3->begin(id3, out);
@@ -74,7 +78,7 @@ void stopMP3()
     file = NULL;
   }
 }
-
+*/
 int servoPulse(int angleDegrees)
 {
   int pulseWidth = map(angleDegrees, 0, 180, 544, 2400);
@@ -92,28 +96,27 @@ int servoSet(int angle)
    return 0;
 }
 
-void crossingClose(bool close)
+void crossingClose()
 {
-  if (close) {
-    //M5.Lcd.println("Close crossing");
-    //M5.Lcd.fillScreen(BLACK);
-    M5.Lcd.fillCircle(100, 100, 50, RED);
-    M5.Lcd.fillCircle(240, 100, 50, RED);
-    //M5.Lcd.fillScreen(RED);
-    for (int i=0; i<=90; i+=3) {
-      servoSet(i);
-    }
+  //M5.Lcd.println("Close crossing");
+  //M5.Lcd.fillScreen(BLACK);
+  M5.Lcd.fillCircle(100, 100, 50, RED);
+  M5.Lcd.fillCircle(240, 100, 50, RED);
+  //M5.Lcd.fillScreen(RED);
+  for (int i=0; i<=90; i+=3) {
+    servoSet(i);
   }
-  else {
-    // open crossing
-    for (int i=90; i>=0; i--)
-      servoSet(i);
-    //M5.Lcd.fillScreen(BLACK);
-    M5.Lcd.fillCircle(100, 100, 50, BLACK);
-    M5.Lcd.fillCircle(240, 100, 50, BLACK);
-    digitalWrite(LED1_PIN, LOW);
-    digitalWrite(LED2_PIN, LOW);
+}
+void crossingOpen()
+{
+  for (int i=90; i>=0; i-=3) {
+    servoSet(i);
   }
+  //M5.Lcd.fillScreen(BLACK);
+  M5.Lcd.fillCircle(100, 100, 50, BLACK);
+  M5.Lcd.fillCircle(240, 100, 50, BLACK);
+  //digitalWrite(LED1_PIN, LOW);
+  //digitalWrite(LED2_PIN, LOW);
 }
 
 void crossingBlink(bool blinker) {
@@ -122,30 +125,30 @@ void crossingBlink(bool blinker) {
     M5.Lcd.fillCircle(100, 100, 50, RED);
     M5.Lcd.fillCircle(240, 100, 50, BLACK);
     //M5.Speaker.tone(442, 10);
-    digitalWrite(LED1_PIN, HIGH);
-    digitalWrite(LED2_PIN, LOW);
+    //digitalWrite(LED1_PIN, HIGH);
+    //digitalWrite(LED2_PIN, LOW);
   }
   else {
     M5.Lcd.fillCircle(100, 100, 50, BLACK);
     M5.Lcd.fillCircle(240, 100, 50, RED);
     //M5.Speaker.tone(440, 10);
-    digitalWrite(LED1_PIN, LOW);
-    digitalWrite(LED2_PIN, HIGH);
+    //digitalWrite(LED1_PIN, LOW);
+    //digitalWrite(LED2_PIN, HIGH);
   }
 }
 
 void setup(){
   M5.begin();
-  WiFi.mode(WIFI_OFF); 
+  //WiFi.mode(WIFI_OFF); 
   delay(500);
   //setupMP3();
   //startMP3();
   
   pinMode(SERVO_PIN, OUTPUT);
-  pinMode(LED1_PIN, OUTPUT);
-  pinMode(LED2_PIN, OUTPUT);
-  digitalWrite(LED1_PIN, LOW);
-  digitalWrite(LED2_PIN, LOW);
+  //pinMode(LED1_PIN, OUTPUT);
+  //pinMode(LED2_PIN, OUTPUT);
+  //digitalWrite(LED1_PIN, LOW);
+  //digitalWrite(LED2_PIN, LOW);
   M5.Lcd.fillCircle(100, 100, 60, TFT_DARKGREY);
   M5.Lcd.fillCircle(240, 100, 60, TFT_DARKGREY);
   delay(3000);
@@ -170,7 +173,7 @@ void loop(){
       // Delay prior to open
       delay(1500);
       //stopMP3();
-      crossingClose(false);
+      crossingOpen();
       closed = false;
     }
   }
@@ -178,7 +181,7 @@ void loop(){
     // Close crossing
     if (distance < 5 || M5.BtnC.wasPressed()) {
       //startMP3();
-      crossingClose(true);
+      crossingClose();
       closed = true;
     }
   }
